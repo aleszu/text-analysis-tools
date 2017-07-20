f92fc33b4470697739a797df3cd52e69099e988a
aleszu <aleszu.bajak@gmail.com>
2017-07-20 13:27:02 -0400

    Add files via upload
    added bigrams and trigrams

--- single-speech-wordcount.R
+++ single-speech-wordcount.R
@@ -17,21 +17,57 @@ tokenizedText <- text_df %>%
  unnest_tokens(word, text) %>%
  anti_join(stop_words)
 
-# Do a wordcount and organize by most frequent 
+# Tokenize text into bigrams and trigrams
+
+bigramsText <- text_df %>%
+  unnest_tokens(bigram,text,token = "ngrams", n = 2) 
+
+trigramsText <- text_df %>%
+  unnest_tokens(trigram,text,token = "ngrams", n = 3) 
+
+# Do a wordcount 
 
 wordCount <- tokenizedText %>%
   count(word, sort = TRUE) 
 
-# View the table of that wordcount
+# Count bigrams and trigrams 
+
+bigramCount <- bigramsText %>%
+  count(bigram, sort = TRUE) 
+
+trigramCount <- trigramsText %>%
+  count(trigram, sort = TRUE) 
+
+# View the tables of bigrams and trigrams
 
 View(wordCount)
+View(bigramCount)
+View(trigramCount)
 
-# Plot table of most frequently used words (with over 10 mentions)
+# Plot most frequently used words (with over 10 mentions)
 
 wordCount %>%
   filter(n > 10) %>%
   mutate(word = reorder(word, n)) %>%
   ggplot(aes(word, n)) +
+  geom_col() +
+  xlab(NULL) +
+  coord_flip()
+
+# Plot most frequently used bigrams and trigrams (with over 5 mentions)
+
+bigramCount %>%
+  filter(n > 10) %>%
+  mutate(bigram = reorder(bigram, n)) %>%
+  ggplot(aes(bigram, n)) +
+  geom_col() +
+  xlab(NULL) +
+  coord_flip()
+
+trigramCount %>%
+  filter(n > 5) %>%
+  mutate(trigram = reorder(trigram, n)) %>%
+  ggplot(aes(trigram, n)) +
   geom_col() +
   xlab(NULL) +
   coord_flip()
